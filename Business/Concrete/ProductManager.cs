03e40268c8a16a41f55f6c385c7ee36423084e4d
Nur B <59769129+nurrbb@users.noreply.github.com>
2021-04-07 22:18:19 +0300

    day 7 lesson notes

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productdal;
+
+        public ProductManager(IProductDal productdal)
+        {
+            _productdal = productdal;
+        }
+
+        public ProductManager(InMemoryProductDal ınMemoryProductDal)
+        {
+        }
+
+        public List<Product> GetAll()
+        {
+            
+        }
+    }
+}
---
+++ ConcoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using System;
+
+namespace ConcoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productmanager = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productmanager.GetAll)
+            {
+                Console.WriteLine(product.);
+            }
+        }
+    }
+}
